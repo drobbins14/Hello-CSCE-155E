515d85f891eb79ff5dd375ab7d2eef1ae96a8049
drobbins14 <42656912+drobbins14@users.noreply.github.com>
2018-08-23 17:25:48 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Derek Robbins and Nicholas 
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Derek and Nicholas!\n");
+
+  return 0;
+}
